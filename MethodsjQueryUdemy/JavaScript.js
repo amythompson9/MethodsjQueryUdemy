a5abcb8bd6abf815d059a570c6b4f0236a39104e
Amy Thompson <amythompson9@gmail.com>
2018-09-20 18:02:16 -0400

    Keypress method

--- MethodsjQueryUdemy/JavaScript.js
+++ MethodsjQueryUdemy/JavaScript.js
@@ -12,3 +12,17 @@ $("button").click(function () {
 //for loop
 //add event listener
 
+//$("input").keypress(function () {
+//    console.log("You pressed a key");
+//});
+
+$("input").keypress(function (event) {
+    if (event.which === 13) {
+        alert("You hit enter");
+    };
+    //console.log(event);
+});
+
+//'which' will be the code of the key that is pressed
+//enter is 13
+

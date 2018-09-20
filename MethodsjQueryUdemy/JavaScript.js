3c95c63ea6009ed7be0b202848b4ee9c49b2f5a8
Amy Thompson <amythompson9@gmail.com>
2018-09-20 17:31:30 -0400

    Click function

--- MethodsjQueryUdemy/JavaScript.js
+++ MethodsjQueryUdemy/JavaScript.js
@@ -1,1 +1,14 @@
-﻿
+﻿$("h1").click(function () {
+    alert("h1 clicked");
+});
+
+$("button").click(function () {
+    $(this).css("background", "pink")
+});
+
+//the above is simpler
+//to do this in javascript
+//query selector all
+//for loop
+//add event listener
+
